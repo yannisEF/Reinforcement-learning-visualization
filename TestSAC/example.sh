365436ad7bd6644183cd580ad726d344bead530f
Yannis <y.elrharbifleury@gmail.com>
2021-04-14 09:49:46 +0200

    Added example.sh to launch the examples

---
+++ TestSAC/example.sh
@@ -0,0 +1,16 @@
+clear
+echo Training the model...
+echo
+python3 trainModel.py --env Pendulum-v0 --max_learn 10000 --save_freq 500 --save_path Models --name_prefix example_pendulum
+clear
+echo Preparing the input policies...
+echo
+python3 preparePolicies.py --inputFolder Models --inputNames "example_pendulum_7000_steps; example_pendulum_7500_steps; example_pendulum_8500_steps; example_pendulum_9000_steps" --outputName "example_around_8000"
+clear
+echo Computing the Vignette...
+echo
+python3 Vignette.py --env Pendulum-v0 --inputDir Models --basename example_pendulum --min_iter 8000 --max_iter 8501 --step_iter 500 --eval_maxiter 1 --nb_lines 10 --policiesPath ComparePolicies/example_around_8000.xz
+clear
+echo Loading the computed Vignette...
+echo
+python3 savedVignette.py --directory SavedVignette --filename example_pendulum
